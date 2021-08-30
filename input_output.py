73ad5e0b0bf5f1a395626b496a3f6ac77462e052
Nolan Taft <killer123yo@gmail.com>
2021-08-30 11:04:16 -0600

    Add files via upload

---
+++ input_output.py
@@ -0,0 +1,7 @@
+# Name variables with input functions.
+first_name = input("What is your first name: ")
+
+last_name = input("What is your last name: ")
+
+# Print functions
+print(("{}, {}").format(last_name, first_name))
